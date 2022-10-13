a08b8c1cdddc9e606e5b7f04d56ec1c99006c787
github_Saikumar8892 <sairoyalsulli@gmail.com>
2022-10-13 21:20:49 +0530

    changes

--- Frontend/frontend-app/src/app/admin-car-retrieve/admin-car-retrieve.component.ts
+++ Frontend/frontend-app/src/app/admin-car-retrieve/admin-car-retrieve.component.ts
@@ -15,12 +15,45 @@ export class AdminCarRetrieveComponent implements OnInit {
   ngOnInit(): void {
     this.findAllCar();
   }
-
+  flag:boolean = false;
+  cid:number =0;
+  cprice:number =0;
+  url:string = "";
   findAllCar(){
    this.cs.findAllCar().subscribe({
     next:(result:any)=>this.cars=result,
     error:(error:any)=>console.log(error),
     complete:()=>console.log("completed")
    })
+  }
+
+  deleteCar(cid:number){
+    //console.log(cid)
+    this.cs.deleteCarById(cid).subscribe({
+      next:(result:any)=>console.log(result),
+      error:(error:any)=>console.log(error),
+      complete:()=>{
+        this.findAllCar();
+      }
+     })
+  }
+
+  updateCar(car:any){
+   this.flag = true;
+   this.cid=car.cid;
+   this.cprice=car.cprice;
+   this.url=car.url;
+  }
+
+  updateDataFromDb(){
+    let product = {cid:this.cid,cprice:this.cprice,url:this.url};
+    this.cs.updateCar(Car).subscribe({
+      next:(result:any)=>console.log(result),
+      error:(error:any)=>console.log(error),
+      complete:()=>{
+        this.findAllCar();
+      }
+     })
+     this.flag=false;
   }
 }
